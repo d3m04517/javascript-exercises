d086f947b80d43d8eb916a9f89957c48382aee8c
Lewis Kim <lewiskim517@gmail.com>
2022-06-10 19:45:21 -0400

    Finished exercises 1-4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr,...indexes) {
+    let newArray = arr;
 
+    for (const index of indexes) {
+        newArray = newArray.filter(e => e !== index);
+    }
+
+    return newArray;
 };
 
 // Do not edit below this line
