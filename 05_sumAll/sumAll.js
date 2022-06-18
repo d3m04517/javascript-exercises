8f7a33516b027d4f9cfa48ee4ecacf0d6cb8eb0d
Lewis Kim <lewiskim517@gmail.com>
2022-06-18 14:37:40 -0400

    Added more efficient algorihthm

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,10 +1,20 @@
 const sumAll = function(num1, num2) {
-    let sum = 0;
+    let n = 0;
 
     if (typeof num1 != "number" || typeof num2 != "number" || num1 < 0 || num2 < 0) {
         return "ERROR";
     }
 
+    if (num1 <= num2) {
+        n = num2 - num1 + 1; //length of the series
+        return n * (num1 + num2) / 2; //formula
+
+    } else {
+        n = num1 - num2 + 1; //length of the series
+        return n * (num1 + num2) / 2; //formula
+    }
+
+    /* Least Efficient Algorithm
     if (num1 <= num2) {
         for (let i = num1; i <= num2; i++) {
             sum += i;
@@ -18,6 +28,8 @@ const sumAll = function(num1, num2) {
 
    
     return sum;
+    */
+
 };
 
 // Do not edit below this line
