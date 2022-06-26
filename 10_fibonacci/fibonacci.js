dd0970081eace41b5df7f9a68859583bc001cae2
Lewis Kim <lewiskim517@gmail.com>
2022-06-26 19:31:26 -0400

    Fibonacci done with loop

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let nums = [1,1];
+    let newNum = 0;
+    if (num < 0) {
+        return "OOPS";
+    } else if (typeof num == String) {
+        newNum = parseInt(num);
+    }
+    for (i = 1; i < num - 1; i++) {
+        nums.push(nums[i] + nums[i-1]);
+    }
+    return nums[nums.length - 1];
 };
 
 // Do not edit below this line
