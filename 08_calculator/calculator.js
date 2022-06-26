a5902ce45389aaed3c996d604782fdce86cbfa3b
Lewis Kim <lewiskim517@gmail.com>
2022-06-26 18:51:39 -0400

    Calculator completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+  let sum = 0;
+	for (let i = 0; i < nums.length; i++) {
+    sum += nums[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  let sum = 1;
+	for (let i = 0; i < nums.length; i++) {
+    sum *= nums[i];
+  }
+  return sum;
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+	let total = 1;
+  for (let i = 1; i <= power; i++) {
+    total *= num;
+  }
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let total = 1;
+	for (let i = 2; i <= num; i++) {
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
