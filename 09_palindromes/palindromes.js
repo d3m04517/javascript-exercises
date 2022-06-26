eab35461b57ffc704b189bbe0ae2ee2df88c5a2c
Lewis Kim <lewiskim517@gmail.com>
2022-06-26 19:06:44 -0400

    Palindromes completed using regexp and loop

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let punctionless = str.toLowerCase().replace(/[!.,\s]/g,"");
+    for (let i = 0; i < punctionless.length; i++) {
+        if (punctionless.charAt(i) != punctionless.charAt(punctionless.length - 1 - i)) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
