10ad94c34090177a44c0ab253ae407a511df3bb7
Lewis Kim <lewiskim517@gmail.com>
2022-07-02 17:41:57 -0400

    findTheOldest algorithm for last test case

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    const oldestPerson = arr.reduce((prev, curr) => {
 
+        if (curr.yearOfDeath && !prev.yearOfDeath) {
+            return prev;
+        } else if (!curr.yearOfDeath && prev.yearOfDeath) {
+            return curr;
+        } else if (!curr.yearOfDeath && !prev.yearOfDeath) {
+            return curr.yearOfBirth < prev.yearOfBirth ? curr : prev;
+        } else {
+        return (curr.yearOfDeath - curr.yearOfBirth) > (prev.yearOfDeath - prev.yearOfBirth) ? curr : prev;
+        }
+    
+    });
+       
+       return oldestPerson;
+       
 };
 
 // Do not edit below this line
