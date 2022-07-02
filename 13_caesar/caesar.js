7ae1c679c1af777422763ee24bc13c5af243ec2e
Lewis Kim <lewiskim517@gmail.com>
2022-07-02 18:55:05 -0400

    Caesar task attempt purely code

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,5 +1,37 @@
-const caesar = function() {
+const caesar = function (word, shift) {
+    let coded = "";
+    for (let i = 0; i < word.length; i++) {
 
+        if (((word.charCodeAt(i)) >= 65) && (word.charCodeAt(i)) <= 90) {
+
+            if ((word.charCodeAt(i) + parseInt(shift)) > 90) {
+                coded += String.fromCharCode((word.charCodeAt(i) + parseInt(shift) - 91) + 65);
+            } else if ((word.charCodeAt(i) + parseInt(shift)) < 65) {
+                coded += String.fromCharCode((word.charCodeAt(i) + parseInt(shift) - 64) + 90);
+            }
+             else {
+                coded += String.fromCharCode(word.charCodeAt(i) + parseInt(shift));
+            }
+        }
+        else if (((word.charCodeAt(i)) >= 97) && ((word.charCodeAt(i)) <= 122)) {
+
+            if ((word.charCodeAt(i) + parseInt(shift)) > 122) {
+                coded += String.fromCharCode(( word.charCodeAt(i) + parseInt(shift) - 123) - 97);
+            } 
+            else if ((word.charCodeAt(i) + parseInt(shift)) < 97) {
+                coded += String.fromCharCode((word.charCodeAt(i) + parseInt(shift) - 96) + 122);
+            }
+            else {
+                coded += String.fromCharCode(word.charCodeAt(i) + parseInt(shift));
+            }
+        }
+        else {
+            
+            coded += String.fromCharCode(word.charCodeAt(i));
+        }
+
+    }
+return coded;
 };
 
 // Do not edit below this line
